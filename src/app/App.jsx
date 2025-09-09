e3094a1422bbbb06cfb8038ff563bfd3b7f585b9
blabss <bo.schroeder20@gmail.com>
2025-09-09 12:28:35 +0200

    test

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-      {/* Your app content */}
+      {/* Your app content goes here*/}
     </BrowserRouter>
   );
 }
