1835d80819ac4585a7b50dcc571f83810e379b2d
blabss <bo.schroeder20@gmail.com>
2025-09-16 10:23:15 +0200

    more changes oops

--- src/features/profile/components/ProfileCard.jsx
+++ src/features/profile/components/ProfileCard.jsx
@@ -4,7 +4,7 @@ export default function ProfileCard({ title, children }) {
   return (
     <div className="relative overflow-hidden rounded-md border border-gray-900 dark:border-gray-300/25 dark:bg-gray-900 bg-stone-50 shadow-sm">
       <div className="bg-gray-800 p-3 pt-2 pb-2">
-        <h2 className="text-sm font-semibold text-gray-900 dark:text-stone-50">
+        <h2 className="text-sm font-semibold dark:text-gray-900 text-stone-50">
           {title}
         </h2>
       </div>
